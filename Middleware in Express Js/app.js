752c5fe5b26a64dd78a3ca76da27d713ae1efa85
KunalWebDevMern <kodshroud@gmail.com>
2024-06-28 19:47:01 +0530

    some-line-added-in-Middleware

--- Middleware in Express Js/app.js
+++ Middleware in Express Js/app.js
@@ -4,7 +4,7 @@
 const express = require('express') ; 
 const app = express();  
 
-app.use(function (req, res, next) {  // is use ka fayada ya hai ki aap ka koi bhi route chlne sei pehle yei chleiga and ikse baad kaam hone ke liye hume  next ka use krna pdeiga taki agei wala kaam ho 
+app.use(function (req, res, next) {  // is use ka fayada ya hai ki aap ka koi bhi route chlne sei pehle yei chleiga and ikse baad kaam hone ke liye hume  next ka use krna pdeiga taki agei wala kaam ho skei ya anyei middle ware pe ja skei
   console.log("hey hello");
   next(); 
 })
